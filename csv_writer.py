cd5547a211efecd58254e8f6c1713c4665aa5798
Martin Bäumer <martin.baeumer@gmail.com>
2024-02-18 23:29:23 +0100

    Reformatted csv writer

--- csv_writer.py
+++ csv_writer.py
@@ -1,12 +1,13 @@
 from endpoint import Endpoint
 
+
 def write_csv_file(filename, endpoints):
-  try:
-    file = open(filename, "w")
-    for p in endpoints:
-      line = p.rest_controller + "," + p.http_method + "," + p.url + "\n"
-      file.write(line)
-    file.close()
-    print("Finished writing data to file")
-  except FileNotFoundError:
-    raise FileNotFoundError
+    try:
+        file = open(filename, "w")
+        for p in endpoints:
+            line = p.rest_controller + "," + p.http_method + "," + p.url + "\n"
+            file.write(line)
+        file.close()
+        print("Finished writing data to file")
+    except FileNotFoundError:
+        raise FileNotFoundError
